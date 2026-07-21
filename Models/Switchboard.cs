12be62db917a33bfcd137d386f9dd6a91c36bfab
Max Safronov <bmc938@gmail.com>
2026-07-21 14:52:11 +0300

    Добавил класс Switchboard, который будет содержать данные об одном щите

---
+++ Models/Switchboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labelman8.Models
+{
+  public class Switchboard
+  {
+    public string SerialNumber { get; set; }
+    public string Name { get; set; }
+    public double In { get; set; }
+    public double Fn { get; set; }
+    public double Um { get; set; }
+    public string Function { get; set; }
+  }
+}
