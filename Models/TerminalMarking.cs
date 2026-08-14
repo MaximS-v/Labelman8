d18f0f5bcc5be6ca73612652aaa910c073a87822
MaximS-v <bmc938@gmail.com>
2026-08-14 13:46:44 +0300

    Добавлены структуры для хранения маркировки для клемм

---
+++ Models/TerminalMarking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Labelman8.Models
+{
+	/// <summary>
+	/// Маркировка для конкретного клеммника
+	/// </summary>
+	public class TerminalMarking
+	{
+		/// <summary>
+		/// Название клеммника (например, "XT1")
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Вид маркировки (ссылка на TerminalMarkingType)
+		/// </summary>
+		public TerminalMarkingType Type { get; set; }
+
+		/// <summary>
+		/// Список обозначений клемм в порядке следования
+		/// </summary>
+		public List<string> Labels { get; set; } = new List<string>();
+
+		/// <summary>
+		/// Количество клемм (определяется по длине Labels)
+		/// </summary>
+		public int TerminalCount => Labels?.Count ?? 0;
+	}
+}
---
+++ Models/TerminalMarkingType.cs
@@ -0,0 +1,23 @@
+﻿namespace Labelman8.Models
+{
+	/// <summary>
+	/// Вид маркировки для клемм (тип клеммника)
+	/// </summary>
+	public class TerminalMarkingType
+	{
+		/// <summary>
+		/// Название вида (например, "Клеммник 5x10")
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Высота наклейки в миллиметрах
+		/// </summary>
+		public double HeightMM { get; set; }
+
+		/// <summary>
+		/// Шаг (ширина одной клеммы) в миллиметрах
+		/// </summary>
+		public double PitchMM { get; set; }
+	}
+}
